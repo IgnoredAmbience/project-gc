627eb859882f3b2ad4c2dc79b39dfe0496262f74
Thomas Wood <thomas.wood09@imperial.ac.uk>
2015-09-01 20:45:44 +0100

    Function for table initialisation and better output

--- calendar.lua
+++ calendar.lua
@@ -8,8 +8,16 @@ Print('Got profile name, ', profileName, "\n")
 -- Get data for as few fields as possible to reduce memory usage. A full list of fields can be found at http://project-gc.com/doxygen/lua-sandbox/classPGC__LUA__Sandbox.html#a8b8f58ee20469917a29dc7a7f751d9ea
 local finds = PGC_GetFinds(profileId, { fields = {'visitdate'} })
 
-local days = {}
-local months = {}
+function inittbl(n, v)
+  t = {}
+  for i = 1, n do
+    t[i] = v
+  end
+  return t
+end
+
+local days = inittbl(31, 0)
+local months = inittbl(12, 0)
 
 local df = Date.Format("yyyy-mm-dd")
 
@@ -18,53 +26,37 @@ for _,f in IPairs(finds) do
   
 	local day = date:day()
     if(date:day() < 30) then
-      days[day] = (days[day] or 0) + 1
+      days[day] = days[day] + 1
     end
   
   	local month = date:month()
-    months[month] = (months[month] or 0) + 1
+    months[month] = months[month] + 1
 end
 
+local tbl = {{"Month", "Finds", "Average"}}
 local monthok = false
 for month = 1, 12 do
   local m = Date { month = month }
   local avg = months[month] / m:last_day():day()
-  Print(m:month_name(), " total: ", months[month], " average: ", avg ,"\n")
+  TableInsert(tbl, {m:month_name(true), months[month], StringFormat("%.2f", avg)})
   monthok = monthok or months[month] > 620
 end
+ 
+local tbl2 = {{"Day", "Finds", "Average"}}
 
 local dayok
 for day = 1, 29 do
   local avg = days[day] / 12
-  Print(day, " total: ", days[day], " average: ", avg ,"\n")
+  TableInsert(tbl2, {day, days[day], StringFormat("%.2f", avg)})
   dayok = dayok or days[day] > 240
 end
 
 
 local ok = monthok and dayok
+local html = "<div style='float: left'>"..PGC_CreateHTMLTable(tbl).."</div><div style='float:left'>"..PGC_CreateHTMLTable(tbl2).."</div><br clear='all'>"
 local log = false
-local html = false
 
 
--- Check if challenge is fulfilled and if so, set ok = true
--- ok = true
-
-if(ok == true) then
-  -- Produce example log, Project-GC recommends using bbcode if it's useful.
-  -- Do not use  log = log .. "foo"  in loops with many iterations, it consumes a lot of memory in LUA.
-  log = {}
-  TableInsert(log, "foo")
-  TableInsert(log, "bar")
-  TableInsert(log, "baz")
-  log = TableConcat(log, "\n")
-end
-
-
---[[
-More feedback can be given by producing html into the html variable
-Can be of good use to show the user his/her progress
-Doesn't actually have to be html
---]]
 
 
 return { ok = ok, log = log, html = html }
